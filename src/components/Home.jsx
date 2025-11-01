eb792d73e35177fe0c1cd5f70c40dda575d0e1eb
Gagan <gaganrdev@gmail.com>
2025-11-01 15:22:36 +0530

    Refactor App component and add Home component with styling updates

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <h1>Reprehenderit in consectetur ut qui. Ipsum irure occaecat Lorem laboris voluptate aliqua esse duis mollit nisi eiusmod. Eu incididunt proident in in. Do magna aute consequat elit qui nisi eu nulla laborum reprehenderit adipisicing. Esse sint eiusmod eu minim ea eiusmod cupidatat nulla in duis laborum do pariatur officia. Velit ipsum dolor enim proident incididunt qui ipsum. Excepteur sunt magna dolore sint eiusmod anim ea laboris pariatur do irure Lorem ullamco.
+
+Labore sit officia esse sit dolore sint exercitation eu non ad. Ullamco ullamco nulla magna cillum non id nulla ex anim. Qui ut reprehenderit dolore cillum ad ad consectetur et cillum aliquip pariatur nisi ad.
+
+Irure Lorem officia irure magna eu irure laboris sint. Nostrud culpa labore consequat aute reprehenderit esse reprehenderit ad nulla. Tempor anim magna adipisicing aliquip ut amet occaecat eu irure nisi. Tempor sit ipsum ut reprehenderit consectetur sit nostrud irure excepteur tempor.
+
+Nostrud incididunt do qui sunt eiusmod mollit. Culpa non eiusmod voluptate pariatur reprehenderit et incididunt proident eu laborum. Minim adipisicing cupidatat pariatur adipisicing nulla quis veniam sunt cupidatat pariatur irure dolor ea.
+
+Dolor cupidatat adipisicing aliqua amet cupidatat proident Lorem aliqua incididunt nisi id aliquip. Nulla do labore dolor esse magna enim est aliquip. Nostrud est eu duis pariatur et veniam laborum ad in excepteur aute. Ea deserunt id veniam aliqua labore est Lorem exercitation velit. Anim ullamco ut veniam labore cillum duis aute magna consectetur.
+
+Nisi nulla qui fugiat culpa in magna amet officia nisi velit. Ullamco adipisicing et aute excepteur culpa. Reprehenderit excepteur occaecat amet do anim. Elit consequat occaecat nisi ut qui. Eiusmod cupidatat nostrud nulla proident id labore.
+
+Quis aliquip sit est irure aliquip. Laboris duis aliquip culpa eu adipisicing. Velit pariatur in pariatur esse. Fugiat nulla laborum eiusmod tempor. Duis id ex ea ex anim consectetur velit Lorem consectetur ullamco.
+
+Ex duis deserunt tempor adipisicing veniam do tempor consequat exercitation ullamco. Ex ipsum dolor reprehenderit culpa in ad irure anim in. Esse pariatur mollit sint est et dolore laborum adipisicing sint anim ut. Ut enim sint minim tempor consequat reprehenderit proident aliqua aute sunt velit incididunt proident. Elit ullamco commodo nostrud reprehenderit exercitation tempor enim aliquip et ex est excepteur ut.
+
+Ullamco anim reprehenderit ex ex fugiat occaecat nisi fugiat mollit ut dolore. Tempor eiusmod deserunt quis adipisicing ipsum laborum nisi officia officia consequat aute ullamco aliqua. Cillum nisi incididunt eiusmod aliquip amet enim aute laborum. Exercitation enim sunt Lorem exercitation in eiusmod sunt et. Veniam quis cillum incididunt minim enim sit nisi anim ullamco est nisi nisi.
+
+Qui commodo velit aliquip cillum duis eiusmod commodo consectetur et. Laboris ad consequat nisi aute cupidatat velit. Cillum reprehenderit officia incididunt ex non occaecat commodo aute nostrud. Tempor fugiat mollit laboris do exercitation. Tempor aliquip mollit nulla incididunt labore cillum do aliquip magna consequat exercitation quis. Do veniam eu sit dolore tempor labore magna reprehenderit sint ea cillum.</h1>
+  )
+}
+
+export default Home
