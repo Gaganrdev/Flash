023096522a2eccf19aae8da1001440987912999a
Gagan <gaganrdev@gmail.com>
2025-11-06 16:04:15 +0530

    Implement routing with Home and Tasks components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import Home from './components/Home';
+import Tasks from './components/Tasks';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="tasks" element={<Tasks />} />
+      </Routes>
+    </Router>
 );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import Button from './UI/button';
+import {Link} from 'react-router-dom';
 
 function Home() {
   return (
     <div className='landing-page'>
         <h1>Start Organizing Your Life</h1>
         <p>Flash helps you declutter your mind and focus on what's truly important. Manage tasks, set priorities, and achieve your goals with ease.</p>
-        <Button text="Get Started - It's Free" />
+        <Link to="/tasks"><Button text="Get Started - It's Free" /></Link>
         <div className='glance'>
             <div>
                 <p>Task today</p>
