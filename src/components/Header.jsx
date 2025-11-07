ccf8b562d4dffaa41c4af3f029362fa7c7ace0c1
Gagan <gaganrdev@gmail.com>
2025-11-07 10:51:26 +0530

    fix: wrap logo in Link component for navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header className='Header'>
         <div className='HeaderTitle'>
-            <img src='logo.svg' alt='Logo'/>
+            <Link to='/'><img src='logo.svg' alt='Logo' /></Link>
             <h1>Flash</h1>
         </div>
         <nav className='HeaderNav'>
